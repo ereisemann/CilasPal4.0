668f485c0581e3346e33ac3922b5d1d1d3d16725
Eve Eisemann <eve.eisemann@gmail.com>
2024-11-05 15:17:37 -0500

    commented out redundant code. leaving commented for now to verify functionality.

--- CilasPal/CilasPal.py
+++ CilasPal/CilasPal.py
@@ -41,6 +41,7 @@ if debug == "Y" or debug == "y":
 else:
     print("Welcome, please paste the path to the data file without quotes (.pdf only!)")   ### it does not seem to accept a file path with quotes ~ere
     file = input("Full file path is: ")
+    file = file.strip('"').strip("'")   ### allows input to include quotes around file path.
 
     pdf = p.PdfReaderObject(file)      ## PDF defined here based on PDF reader object (see PdfReaderObj.py)
 
@@ -57,75 +58,75 @@ else:
         mean = contents1[contents1.index('Mean diameter') + 16:contents1.index('FraunhoferDensity')-4]
 
         print(sample_name)  ## keep in some verbose for regular script
-
-        # Check index alignment on first sample
-        if i == 0:
-            print(Fore.YELLOW + "...Testing Index Alignment on sample 1...\n" + Fore.WHITE)
-            print(f"name:{sample_name}mean: {mean}\nmedian: {median}")
-
-            if " " in sample_name or " " in mean or " " in median: # If there are spaces before/after
-                #raise Exception("Misalignment in indexing size metrics. Check that pdf version == or that the sample name does not contain spaces.")   ### Now that indexing is based on a cleaned version of all the values, not sure spaces in sample names are a problem
-                warnings.warn("Misalignment in indexing size metrics. Check that pdf version == or that the sample name does not contain spaces.")   ### Now that indexing is based on a cleaned version of all the values, not sure spaces in sample names are a problem
-            else:
-                print(Fore.GREEN + "Correct indexing size metrics ... checking size classes..." + Fore.WHITE)
-
-                contents1 = contents1[620:]
-
-                defined_class_distrib = []
-                for classs in defined_classes_headers:
-                    s = contents1.index(classs)
-                    try:
-                        val = float(contents1[s+6:s+11])     # Catches if output contains characters --> can not convert a misalignment to float
-                        defined_class_distrib.append(val)
-                    except:
-                        print(Fore.RED + f"Misalignment in indexing size classes. Can not cast {contents1[s+6:s+11]} to float" + Fore.WHITE)
-                        raise Exception("Misalignment in indexing size classes. Can not cast to float. see above")
-                print(Fore.GREEN + "Correct indexing for customer defined size classes ... checking standard classes..." + Fore.WHITE)
-
-
-    ### major update below ~ere
-                search_pattern_1 = '\nx\nQ3\nq3'    ### start ### improved string trimming based on pattern ~ere
-                search_pattern_2 = 'diameter'   ### end ###
-                if search_pattern_1 and search_pattern_2 in contents2:
-                    start_index = contents2.index(search_pattern_1) ### first occurrence pattern 1
-                    end_index = contents2.rfind(search_pattern_2) ### last occurrence of pattern 2
-                    trimmed_txt = contents2[start_index:end_index] ### Trim everything outside those
-                    trimmed_text = trimmed_txt.replace('Q3', ' ').replace('q3', ' ')
-                else:
-                    trimmed_text = contents2   ### Result if no patterns found, probably unnecessary
-
-                cleaned_text = re.findall(r'[0-9]+\.[0-9]+(?:\.[0-9]+)?', trimmed_text)  ### isolating numbers, decimals, spaces
-
-                def split_long_strings(data):    ### Function to split numbers that are mashed together. ~ere
-                    result = []
-                    for item in data:
-                        ### If the string is longer than 5 characters, split
-                        if len(item) > 6 and item != '100.00':  ### where item length exceeds 6 and are not 100.00
-                            ### Check if the string contains a decimal after the first character
-                            if item[1] == '.':
-                                split_point = 4
-                            else:
-                                split_point = 3
-                            ### Split the string at the calculated point and append both parts to result
-                            result.append(item[:split_point])
-                            result.append(item[split_point:])
-                        else:  ### when the string is 5 characters or less or equal to 100.00, keep unsplit
-                            result.append(item)
-                    ### Return the list of strings, excluding spaces
-                    return [s for s in result if s.strip()]
-
-                split_text = split_long_strings(cleaned_text)   ### reassign contents2 to the final version of the list of strings
-    ### major update above ~ere
-
-                standard_class_distrib = []
-                for j in range(1, len(standard_classes_headers)):  ### sometimes class header shows up in the data before appropriate location, causing problems with this indexing method
-                    try:
-                        val = float(split_text[j * 3 - 1])
-                        standard_class_distrib.append(val)
-                    except:
-                        print(Fore.RED + f"Misalignment in indexing size classes. Can not cast {split_text[j * 3 - 1]} to float" + Fore.WHITE)  ### ~ere
-                        raise Exception("Misalignment in indexing size classes. Can not cast to float, see above")
-                print(Fore.GREEN + "Correct indexing for standard size classes ... proceeding to parse full data..." + Fore.WHITE)
+    #
+    #     # Check index alignment on first sample
+    #     if i == 0:
+    #         print(Fore.YELLOW + "...Testing Index Alignment on sample 1...\n" + Fore.WHITE)
+    #         print(f"name:{sample_name}mean: {mean}\nmedian: {median}")
+    #
+    #         if " " in sample_name or " " in mean or " " in median: # If there are spaces before/after
+    #             #raise Exception("Misalignment in indexing size metrics. Check that pdf version == or that the sample name does not contain spaces.")   ### Now that indexing is based on a cleaned version of all the values, not sure spaces in sample names are a problem
+    #             warnings.warn("Misalignment in indexing size metrics. Check that pdf version == or that the sample name does not contain spaces.")   ### Now that indexing is based on a cleaned version of all the values, not sure spaces in sample names are a problem
+    #         else:
+    #             print(Fore.GREEN + "Correct indexing size metrics ... checking size classes..." + Fore.WHITE)
+    #
+    #             contents1 = contents1[620:]
+    #
+    #             defined_class_distrib = []
+    #             for classs in defined_classes_headers:
+    #                 s = contents1.index(classs)
+    #                 try:
+    #                     val = float(contents1[s+6:s+11])     # Catches if output contains characters --> can not convert a misalignment to float
+    #                     defined_class_distrib.append(val)
+    #                 except:
+    #                     print(Fore.RED + f"Misalignment in indexing size classes. Can not cast {contents1[s+6:s+11]} to float" + Fore.WHITE)
+    #                     raise Exception("Misalignment in indexing size classes. Can not cast to float. see above")
+    #             print(Fore.GREEN + "Correct indexing for customer defined size classes ... checking standard classes..." + Fore.WHITE)
+    #
+    #
+    # ### major update below ~ere
+    #             search_pattern_1 = '\nx\nQ3\nq3'    ### start ### improved string trimming based on pattern ~ere
+    #             search_pattern_2 = 'diameter'   ### end ###
+    #             if search_pattern_1 and search_pattern_2 in contents2:
+    #                 start_index = contents2.index(search_pattern_1) ### first occurrence pattern 1
+    #                 end_index = contents2.rfind(search_pattern_2) ### last occurrence of pattern 2
+    #                 trimmed_txt = contents2[start_index:end_index] ### Trim everything outside those
+    #                 trimmed_text = trimmed_txt.replace('Q3', ' ').replace('q3', ' ')
+    #             else:
+    #                 trimmed_text = contents2   ### Result if no patterns found, probably unnecessary
+    #
+    #             cleaned_text = re.findall(r'[0-9]+\.[0-9]+(?:\.[0-9]+)?', trimmed_text)  ### isolating numbers, decimals, spaces
+    #
+    #             def split_long_strings(data):    ### Function to split numbers that are mashed together. ~ere
+    #                 result = []
+    #                 for item in data:
+    #                     ### If the string is longer than 5 characters, split
+    #                     if len(item) > 6 and item != '100.00':  ### where item length exceeds 6 and are not 100.00
+    #                         ### Check if the string contains a decimal after the first character
+    #                         if item[1] == '.':
+    #                             split_point = 4
+    #                         else:
+    #                             split_point = 3
+    #                         ### Split the string at the calculated point and append both parts to result
+    #                         result.append(item[:split_point])
+    #                         result.append(item[split_point:])
+    #                     else:  ### when the string is 5 characters or less or equal to 100.00, keep unsplit
+    #                         result.append(item)
+    #                 ### Return the list of strings, excluding spaces
+    #                 return [s for s in result if s.strip()]
+    #
+    #             split_text = split_long_strings(cleaned_text)   ### reassign contents2 to the final version of the list of strings
+    # ### major update above ~ere
+    #
+    #             standard_class_distrib = []
+    #             for j in range(1, len(standard_classes_headers)):  ### sometimes class header shows up in the data before appropriate location, causing problems with this indexing method
+    #                 try:
+    #                     val = float(split_text[j * 3 - 1])
+    #                     standard_class_distrib.append(val)
+    #                 except:
+    #                     print(Fore.RED + f"Misalignment in indexing size classes. Can not cast {split_text[j * 3 - 1]} to float" + Fore.WHITE)  ### ~ere
+    #                     raise Exception("Misalignment in indexing size classes. Can not cast to float, see above")
+    #             print(Fore.GREEN + "Correct indexing for standard size classes ... proceeding to parse full data..." + Fore.WHITE)
 
 
         # Same exact thing as above, still catches misalignment errors but has no verbose output   ### Commenting this out b/c unneeded ~ere
@@ -193,7 +194,6 @@ else:
                     raise Exception("Misalignment in indexing size classes. Can not cast to float, see above")
 
 
-
             workbook = openpyxl.load_workbook(spreadsheet_path)
             defined_class_distrib_sheet = workbook[workbook.sheetnames[1]]
             standard_class_distrib_sheet = workbook[workbook.sheetnames[0]]
@@ -210,5 +210,3 @@ else:
             defined_class_distrib_sheet.cell(row=row, column=j+2, value=val)
         workbook.save(spreadsheet_path)
         row+=1
-            
-
